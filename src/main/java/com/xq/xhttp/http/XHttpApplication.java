7bcddffe1979b9b6d6158dd5033ad6d69bf6a609
XQ <1379716219@qq.com>
2021-03-24 23:59:21 +0800

    添加主函数 问题：springboot项目打包成依赖包为什么需要主函数？

---
+++ src/main/java/com/xq/xhttp/http/XHttpApplication.java
@@ -0,0 +1,13 @@
+package com.xq.xhttp.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XHttpApplication.class, args);
+    }
+
+}
