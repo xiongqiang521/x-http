57d4ec3510ec9e84826896316daf0be753c391cd
XQ <1379716219@qq.com>
2021-03-28 19:09:42 +0800

    自动装配，导入依赖包后自动启用

--- src/main/java/com/xq/xhttp/http/annotation/HttpApiScan.java
+++ src/main/java/com/xq/xhttp/http/annotation/HttpApiScan.java
@@ -2,7 +2,6 @@ package com.xq.xhttp.http.annotation;
 
 
 import com.xq.xhttp.http.handler.HttpApiScanRegister;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
@@ -11,7 +10,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Import(HttpApiScanRegister.class)
-@ComponentScan("com.xq.xhttp.http.handler")
 public @interface HttpApiScan {
 
     String[] value() default {};
