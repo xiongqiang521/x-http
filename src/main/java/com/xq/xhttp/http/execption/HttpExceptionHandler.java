882c6417f8ca398f6510fd4d3d7a9262bbbaaed7
xiongqiang <1379716219@qq.com>
2022-07-21 15:51:22 +0800

    减少不必要依赖

--- src/main/java/com/xq/xhttp/http/execption/HttpExceptionHandler.java
+++ src/main/java/com/xq/xhttp/http/execption/HttpExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.xq.xhttp.http.execption;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
@@ -9,13 +7,14 @@ import org.springframework.web.client.ResponseErrorHandler;
 import java.io.IOException;
 import java.net.URI;
 
+/**
+ * RestTemplate 异常处理
+ */
 public class HttpExceptionHandler implements ResponseErrorHandler {
-    private final static Logger logger = LoggerFactory.getLogger(HttpExceptionHandler.class);
 
     @Override
     public void handleError(URI url, HttpMethod method, ClientHttpResponse response) throws IOException {
         ResponseErrorHandler.super.handleError(url, method, response);
-        logger.error("remote http fail. => {} {}", method, url);
         throw new XHttpException(ExceptionEnum.REMOTE_EXCEPTION);
     }
 
